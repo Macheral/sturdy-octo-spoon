a5d421a195ac9e13aa1845ef81d338c0c18ed8e1
KahJune <JUNEMKJ85.23@ichat.sp.edu.sg>
2026-05-17 19:17:53 +0800

    Added CardView and Google ML Text Recognition Kit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation ("com.google.mlkit:text-recognition:16.0.0")
+
 }
